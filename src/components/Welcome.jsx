d44167a28c2e6db29c78b54390ec021e89e7c316
Parth_ <akhileshshukla2901@gmail.com>
2026-04-26 10:59:07 +0530

    Minor Changes

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -20,7 +20,7 @@ const renderText = (text, className, baseWeight = 400) => {
 };
 
 const setupTextHover = (container, type) => {
-  if (!container) return;
+  if (!container) return () => {};
 
   const animateLetter = (letter, weight, duration = 0.25) => {
     return gsap.to(letter, {
